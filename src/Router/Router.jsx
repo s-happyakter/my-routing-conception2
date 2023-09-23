bde1a80d412de3a546782af8db4202b325ec6918
s-happyakter <saydahappyakter@gmail.com>
2023-09-23 18:35:28 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./layout/pages/Home/Home";
+import Favorites from "./layout/pages/Favorites/Favorites";
+import Login from "./layout/pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/favorites',
+            element: <Favorites></Favorites>
+        },
+        {
+            path:'/login',
+            element:<Login></Login>
+        }
+      ]
+    },
+  ]);
+
+export default router;
---
+++ src/Router/layout/pages/Favorites/Favorites.jsx
@@ -0,0 +1,11 @@
+
+
+const Favorites = () => {
+    return (
+        <div>
+             <h1>hello Favorites</h1>
+        </div>
+    );
+};
+
+export default Favorites;
