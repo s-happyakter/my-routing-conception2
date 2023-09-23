70587d6a58600d78e168952d8fffa52439858b73
s-happyakter <saydahappyakter@gmail.com>
2023-09-23 19:17:41 +0600

    compler navbar

--- src/Router/layout/MainLayout.jsx
+++ src/Router/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 
 import { Outlet } from 'react-router-dom';
-import Navbar from './component/Header/Navbar';
+import Navbar from '../../component/Header/Navebar/Navbar';
+
 
 const MainLayout = () => {
     return (
---
+++ src/component/Header/Navebar/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Logo = () => {
+    return (
+        <div>
+            <h1 className='text-xl font-semibold'>Phone Shop</h1>
+        </div>
+    );
+};
+
+export default Logo;
